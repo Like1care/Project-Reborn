6f70fc3a859e4f81dbd618403bcc7f5a7f97999d
0care <pckbrk@gmail.com>
2024-05-31 10:25:48 +0500

    Stash version for recover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -25,3 +25,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    
